eadd8cb3a4ee2e6ed13e2b62639b711b56207eb0
Philipp Sum <sum.philipp@gmail.com>
2024-11-09 16:53:56 +0100

    update
    added frontend and react router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,52 @@
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Home from './routes/Home';
+import About from './routes/About';
+import NoMatch from './routes/noMatch';
+
+export default function App() {
+  return (
+    <div>
+      <h1>File-Uploader</h1>
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
